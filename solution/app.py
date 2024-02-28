0ca4f9704f93cebc8218cbb063843a3bfdd36672
Michael <ostaninmiha7@gmail.com>
2024-02-28 12:56:47 +0300

    Added countries api

--- solution/app.py
+++ solution/app.py
@@ -1,4 +1,8 @@
 from flask import Flask, request, jsonify
+from config import SERVER_ADDRESS, SERVER_PORT
+from database import start_session, start_engine
+from sqlalchemy import text
+
 
 app = Flask(__name__)
 
@@ -6,5 +10,21 @@ app = Flask(__name__)
 def send():
     return jsonify({"status": "ok"}), 200
 
+
+@app.route('/api/countries', methods=["GET"])
+def list_countries():
+    if not request.args.get('region') == None:
+        with engine.connect() as connection:
+            result = connection.execute(text(f"SELECT name FROM countries WHERE region = '{request.args.get('region').capitalize()}'")).all()
+            
+            return jsonify([i[0] for i in result])
+    else:
+        with engine.connect() as connection:
+            result = connection.execute(text(f"SELECT name FROM countries")).all()
+            return jsonify([i[0] for i in result])
+
+
+
 if __name__ == "__main__":
-    app.run()
+    engine = start_engine()
+    app.run(host=SERVER_ADDRESS, port=SERVER_PORT, debug=True)
---
+++ solution/database.py
@@ -0,0 +1,29 @@
+import psycopg2
+from config import POSTGRES_CONN, DEBUG, POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DATABASE
+from sqlalchemy import Engine, create_engine, text
+from sqlalchemy.orm import sessionmaker
+
+
+def start_engine() -> Engine:
+    try:
+        engine = create_engine(f"postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}")
+    except Exception as e:
+        raise ConnectionError("Не удалось подлкючиться к Базе Данных.")
+    return engine
+
+
+def start_session():
+    try:
+        engine = start_engine()
+        session = sessionmaker(bind=engine)
+    except Exception as e:
+        raise ConnectionError("Не удалось создать сессию.")
+    return session
+
+
+if __name__ == "__main__":
+    session = start_session()
+    engine = start_engine()
+    # with engine.connect() as connection:
+    #         result = connection.execute(text(f"SELECT name FROM countries WHERE region = '{request.args.get('region').capitalize()}'")).all()
+    #         print(f"[*]                            {[i[0] for i in result]}")
