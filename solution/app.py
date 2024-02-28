0c36a7a6a30d2eae52fc8ba6c42a79e3e9ca2626
Michael <ostaninmiha7@gmail.com>
2024-02-28 14:05:35 +0300

    Completed countries API feature

--- solution/app.py
+++ solution/app.py
@@ -14,17 +14,25 @@ def send():
 @app.route('/api/countries', methods=["GET"])
 def list_countries():
     if not request.args.get('region') == None:
-        with engine.connect() as connection:
-            result = connection.execute(text(f"SELECT name FROM countries WHERE region = '{request.args.get('region').capitalize()}'")).all()
-            
-            return jsonify([i[0] for i in result])
+            with engine.connect() as connection:
+                result = connection.execute(text(f"SELECT name FROM countries WHERE region = '{request.args.get('region').capitalize()}'")).all()
+                return jsonify([i[0] for i in result]), 200
     else:
         with engine.connect() as connection:
             result = connection.execute(text(f"SELECT name FROM countries")).all()
-            return jsonify([i[0] for i in result])
+            return jsonify([i[0] for i in result]), 200
+
+
+@app.route('/api/countries/<alpha2>', methods=['GET'])
+def get_country_by_alpha2(alpha2):
+    with engine.connect() as connection:
+        print(alpha2)
+        result = connection.execute(text(f"SELECT name, alpha2, alpha3, region FROM countries WHERE alpha2 = '{alpha2.upper()}'")).all()
+        print(result)
+        return jsonify([i for i in result[0]]), 200
 
 
 
 if __name__ == "__main__":
     engine = start_engine()
-    app.run(host=SERVER_ADDRESS, port=SERVER_PORT, debug=True)
+    app.run(host=SERVER_ADDRESS, port=SERVER_PORT, debug=False)
